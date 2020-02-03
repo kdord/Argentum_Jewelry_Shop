d23800da05a62b7febaea48e7a829d395cf31e2d
kdord <katya.koliada49@gmail.com>
2020-02-03 11:02:38 +0200

    delete modals

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -4,113 +4,14 @@ import instaLogo from '../images/logoInstagram.png';
 import fbLogo from '../images/fbLogo.png';
 import '../style/css/HeaderStyle.css';
 import { Link } from 'react-router-dom';
-import { Dropdown, Modal, Button, Form } from 'react-bootstrap';
+import { Dropdown } from 'react-bootstrap';
 import { FaUser } from 'react-icons/fa';
 
-class LoginModal extends Component {
-  render() {
-    return (
-      <Modal
-        {...this.props}
-        size='lg'
-        aria-labelledby='contained-modal-title-vcenter'
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id='contained-modal-title-vcenter'>Увійти</Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group>
-              <Form.Label>Адреса електронної пошти</Form.Label>
-              <Form.Control
-                name='email'
-                type='email'
-                placeholder='Enter your email'
-              />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Пароль</Form.Label>
-              <Form.Control
-                name='password'
-                type='password'
-                placeholder='Enter your password'
-              />
-            </Form.Group>
-            <Button variant='secondary' type='submit'>
-              Увійти
-            </Button>
-          </Form>
-        </Modal.Body>
-      </Modal>
-    );
-  }
-}
-class SignUpModal extends Component {
-  render() {
-    return (
-      <Modal
-        {...this.props}
-        size='lg'
-        aria-labelledby='contained-modal-title-vcenter'
-        centered
-      >
-        <Modal.Header closeButton>
-          <Modal.Title id='contained-modal-title-vcenter'>
-            Реєстрація
-          </Modal.Title>
-        </Modal.Header>
-        <Modal.Body>
-          <Form>
-            <Form.Group>
-              <Form.Label>Ім'я та прізвище</Form.Label>
-              <Form.Control type='text' placeholder='Enter your name' />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Адреса електронної пошти</Form.Label>
-              <Form.Control type='email' placeholder='Enter your email' />
-            </Form.Group>
-            <Form.Group>
-              <Form.Label>Пароль</Form.Label>
-              <Form.Control type='password' placeholder='Enter your password' />
-            </Form.Group>
-            <Button variant='secondary' type='submit'>
-              Реєстрація
-            </Button>
-          </Form>
-        </Modal.Body>
-      </Modal>
-    );
-  }
-}
-
 class Header extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      modalLoginShow: false,
-      modalSignUpShow: false
-    };
-    this.handleToggleLogin = this.handleToggleLogin.bind(this);
-    this.handleToggleSignUp = this.handleToggleSignUp.bind(this);
-  }
-
-  handleToggleLogin() {
-    this.setState({
-      modalLoginShow: !this.state.modalLoginShow
-    });
-  }
-  handleToggleSignUp() {
-    this.setState({
-      modalSignUpShow: !this.state.modalSignUpShow
-    });
-  }
-
   render() {
     const instaURL = 'https://www.instagram.com/argentum_jewelry_shop/';
     const fbURL = 'https://www.facebook.com/argentumjewelry47/';
-    const { modalLoginShow, modalSignUpShow } = this.state;
+
     return (
       <div className='header'>
         <div className='header-top'>
@@ -129,20 +30,10 @@ class Header extends Component {
               </Dropdown.Toggle>
 
               <Dropdown.Menu>
-                <Dropdown.Item onClick={this.handleToggleLogin}>
-                  Login
-                </Dropdown.Item>
-                <Dropdown.Item onClick={this.handleToggleSignUp}>
-                  Sign Up
-                </Dropdown.Item>
+                <Dropdown.Item href='/login'>Login</Dropdown.Item>
+                <Dropdown.Item href='/registration'>Sign Up</Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
-
-            <LoginModal show={modalLoginShow} onHide={this.handleToggleLogin} />
-            <SignUpModal
-              show={modalSignUpShow}
-              onHide={this.handleToggleSignUp}
-            />
           </div>
         </div>
         <div className='header-bottom'>
