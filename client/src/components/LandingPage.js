32222851d85349a9b57c2b8a62919e138625038c
kdord <katya.koliada49@gmail.com>
2020-02-11 17:26:00 +0200

    add Carousel in Landing Page

--- client/src/components/LandingPage.js
+++ client/src/components/LandingPage.js
@@ -1,10 +1,24 @@
 import React, { Component } from 'react';
+import '../style/css/LandingPageStyle.css';
+
+import banner2 from '../images/banner2.png';
+import banner1 from '../images/banner1.png';
+import { Carousel } from 'react-bootstrap';
 
 export default class LandingPage extends Component {
   render() {
     return (
-      <div>
-        <h1>Landing Page</h1>
+      <div className='landing'>
+        <div className='banner'>
+          <Carousel indicators={false} interval={3000}>
+            <Carousel.Item>
+              <img className='d-block w-100' src={banner1} alt='banner' />
+            </Carousel.Item>
+            <Carousel.Item>
+              <img className='d-block w-100' src={banner2} alt='banner' />
+            </Carousel.Item>
+          </Carousel>
+        </div>
       </div>
     );
   }
