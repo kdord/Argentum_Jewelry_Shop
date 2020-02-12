50ff55015072e545efeeddf02ede82f9f69031a2
kdord <katya.koliada49@gmail.com>
2020-02-12 17:41:31 +0200

    edit .gitignore

---
+++ client/build/precache-manifest.e6158b722cd6b3b897e95b7b33db093f.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fe9e75f2575ed146c5b5abe06681a617",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6745c697a3d94b01f030",
+    "url": "/static/css/2.67123b20.chunk.css"
+  },
+  {
+    "revision": "5aef81b7aebd07f4b2d3",
+    "url": "/static/css/main.9ad238d8.chunk.css"
+  },
+  {
+    "revision": "6745c697a3d94b01f030",
+    "url": "/static/js/2.a36efd1e.chunk.js"
+  },
+  {
+    "revision": "86f96943d6537349861c4002a14026e3",
+    "url": "/static/js/2.a36efd1e.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5aef81b7aebd07f4b2d3",
+    "url": "/static/js/main.3ba34929.chunk.js"
+  },
+  {
+    "revision": "5d93c6b2d15332364552",
+    "url": "/static/js/runtime-main.cea588d5.js"
+  },
+  {
+    "revision": "b9fd505eee77355e92bf380c0ff222d6",
+    "url": "/static/media/banner1.b9fd505e.png"
+  },
+  {
+    "revision": "b0632384c53eacec5528a94b4bfaa0d2",
+    "url": "/static/media/banner2.b0632384.png"
+  },
+  {
+    "revision": "87cf7d60f2ee8a20f6e9edc59b359b6c",
+    "url": "/static/media/bracelets.87cf7d60.png"
+  },
+  {
+    "revision": "555f125b6c261dbda2adc86baa18081c",
+    "url": "/static/media/earrings.555f125b.png"
+  },
+  {
+    "revision": "c96a498312e9cd820364a82da14bf43a",
+    "url": "/static/media/fbLogo.c96a4983.png"
+  },
+  {
+    "revision": "42ed39f809d0c6453898053eb34fcf8a",
+    "url": "/static/media/logoNew.42ed39f8.png"
+  },
+  {
+    "revision": "336f84aa395abf7623562a29c883e5be",
+    "url": "/static/media/neckleces.336f84aa.png"
+  },
+  {
+    "revision": "fa89ef6c42772ef3a47935e816d7dfe7",
+    "url": "/static/media/rings.fa89ef6c.png"
+  }
+]);
